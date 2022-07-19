844a9769b4000b9952647f3aef24804909f1f8c6
dmohapatra11 <the.creator.dm@gmail.com>
2022-07-19 20:55:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-dzhbln
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-dzhbln)
---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular';
+  matrix = [
+    [0, 0, 0],
+    [1, 1, 0],
+    [0, 0, 0],
+    [0, 1, 1],
+    [0, 0, 0],
+  ];
+}
